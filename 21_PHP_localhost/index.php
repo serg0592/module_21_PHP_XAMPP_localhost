bfced3599d2246e45f427c819e30df51409a67a4
Sergey Kudelin <kudelin.work@bk.ru>
2022-02-02 22:14:02 +0300

    Updated: index; Added: main

--- 21_PHP_localhost/index.php
+++ 21_PHP_localhost/index.php
@@ -1,64 +1,14 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="css/style2.css">
-        <link href="https://fonts.googleapis.com/css2?family=Revalia&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
-        <title>PHP practice</title>
-    </head>
-    <body>
-        <header></header>
-        <main>
-            <div class="grid">                
-                <div class="item box1">
-                    <?php include 'logo.inc.php' ?>
-                </div>
-                <div class="item box2">
-                    <?php include 'menu.inc.php' ?>
-                </div>
-                <div class="item box3">
-                    <?php include 'frame.inc.php' ?>
-                </div>                  
-                <div class="item box4">
-                    <?php include 'write_to_me.inc.php' ?>
-                </div>
-                <div class="item box5">
-                    <?php include 'about_me.inc.php' ?>
-                </div>
-                <div class="item box6">
-                    <?php include 'numbers.inc.php' ?>
-                </div>
-                <div class="item box7">
-                    <?php include 'skills.inc.php' ?>
-                </div>
-                <div class="item box8">
-                    <?php include 'photo.inc.php' ?>
-                </div>                  
-                <div class="item box9">
-                    <?php include 'video.inc.php' ?>
-                </div>
-                <div class="item box10">
-                    <?php include 'gallery.inc.php' ?>
-                </div>
-                <div class="item box11">
-                    <?php include 'clients.inc.php' ?>
-                </div>
-                <div class="item box12">
-                    <?php include 'form.inc.php' ?>
-                </div>
-                <div class="item box13">
-                    <?php include 'copyright.inc.php' ?>
-                </div>
-                <div class="item box14">
-                    <?php include 'soc_logo.inc.php' ?>
-                </div>
-            </div>
-        </main>
-        <footer></footer>
-    </body>
+<?php
+    $p = `Hi there on my first PHP project`;
+?>
 
-<script src="script.js"></script>
-</html>
+<?php
+    $firstname = `Sergey`;
+    $lastname = `Kudelin`;
+    $city = `Moscow`;
+    $age = `29`;
+?>
+
+<?php
+    include 'main.php';
+?>
