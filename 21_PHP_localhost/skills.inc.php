e5ef4e4197c20e0bb4aeaed842c824db57fd8811
Sergey Kudelin <kudelin.work@bk.ru>
2022-02-03 00:48:31 +0300

    Updated: skills

--- 21_PHP_localhost/skills.inc.php
+++ 21_PHP_localhost/skills.inc.php
@@ -1,5 +1,14 @@
 <div class="skills">
-    <?php echo 'skills_content.inc.php' . '<br>'; ?>
+    <h2>Мои навыки</h2>                        
+    <p>Adobe Photoshop</p>
+    <div class="skill1"></div>
+    <div class="skill1_2"></div>
+    <p>Adobe Photoshop</p>
+    <div class="skill2"></div>
+    <div class="skill2_2"></div>
+    <p>Adobe Photoshop</p>
+    <div class="skill3"></div>
+    <div class="skill3_2"></div>
     <?php echo $a, ' ', $b, ' ', $c . '<br>'; ?>
     <?php
         $a = 23;
